9583ba6484c1b5af5f5c1d8732471cdfc716415a
Giang Nguyen <hoanggia@uni-bremen.de>
2025-07-27 17:43:12 +0200

    Add laser_scan

--- src/multiverse_ros_socket/multiverse_node/multiverse_publishers/__init__.py
+++ src/multiverse_ros_socket/multiverse_node/multiverse_publishers/__init__.py
@@ -2,3 +2,4 @@ from .tf_publisher import TfPublisher
 from .odom_publisher import OdomPublisher
 from .image_rgb_publisher import ImageRgbPublisher
 from .image_depth_publisher import ImageDepthPublisher
+from .laser_scan_publisher import LaserScanPublisher
