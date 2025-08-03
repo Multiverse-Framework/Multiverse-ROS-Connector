e31503a789ba7e1e1d68d1400b93f06344057526
Giang Nguyen <hoanggia@uni-bremen.de>
2025-08-03 14:33:25 +0200

    Add pose_stamped_subscriber

--- src/multiverse_ros_socket/multiverse_node/multiverse_subscribers/__init__.py
+++ src/multiverse_ros_socket/multiverse_node/multiverse_subscribers/__init__.py
@@ -2,3 +2,4 @@ from .cmd_vel_subscriber import CmdVelSubscriber
 from .data_subscriber import DataSubscriber
 from .joint_state_subscriber import JointStateSubscriber
 from .tf_subscriber import TfSubscriber
+from .pose_stamped_subscriber import PoseStampedSubscriber
